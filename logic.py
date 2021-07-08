b91c777b1b7d85cbfcf262ccaacfc109ec29d96b
Ajharabdullah <86344620+Ajharabdullah@users.noreply.github.com>
2021-07-08 17:17:44 +0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -10,6 +10,7 @@ class calc:
 		self.input = self.e.get()
 		self.display = self.input.replace('÷','/')
 		self.display = self.display.replace('x','*')
+		self.display = self.display.replace('^', "**")
 
 	def button_equal(self):
 		self.operation()
@@ -36,6 +37,50 @@ class calc:
 			self.txt=self.e.get()[:-1]
 			self.e.delete(0,END)
 			self.e.insert(0,self.txt)
+	
+	def squareroot(self):
+		"""squareroot method"""
+		self.operation()
+		try:
+			# evaluate the expression using the eval function
+			self.value= eval(self.display)
+
+		except SyntaxError or NameError:
+			self.e.delete(0,END)
+			self.e.insert(0,'Enter Value First')
+		else:
+			self.sqrtval=math.sqrt(self.value)
+			ans = self.sqrtval
+			if isinstance (ans, float):
+				if ans - int(ans) == 0:
+					answer = round(ans)
+					self.e.delete(0,END)
+					self.e.insert(0,answer)
+				else:
+					self.e.delete(0,END)
+					self.e.insert(0,ans)
+
+	def square(self):
+		"""square method"""
+		self.operation()
+		try:
+			#evaluate the expression using the eval function
+			self.value = eval(self.display)
+		except SyntaxError or NameError:
+			self.e.delete(0,END)
+			self.e.insert(0,'Enter Value First')
+		else:
+			self.sqval=math.pow(self.value,2)
+			ans = self.sqval
+			if isinstance (ans, float):
+				if ans - int(ans) == 0:
+					answer = round(ans)
+					self.e.delete(0,END)
+					self.e.insert(0,answer)
+				else:
+					self.e.delete(0,END)
+					self.e.insert(0,ans)
+
 			
 # Driver Code
 root = Tk()
